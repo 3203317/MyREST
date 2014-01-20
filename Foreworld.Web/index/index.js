434995f802f190ad26c29600559aca6cfe0a77da
3203317 <3203317@qq.com>
2014-01-20 11:21:43 +0800

    haha

--- Foreworld.Web/index/index.js
+++ Foreworld.Web/index/index.js
@@ -1,20 +1,4 @@
-﻿/**
- * 作者：黄鑫
- * 日期：2013-08-24
- * 描述：首页
- */
-var console = console||{log:function(){return;}}
-require.config(config);
-require(["jquery",
-	"util",
-	"footer"], function($, $util, $footer){
-
-	$(document).ready(function(){
-		$footer();
-	});
-},function($err){    
-	var _failedId = $err.requireModules && $err.requireModules[0];
-    if (_failedId === "jquery"){
-        requirejs.undef(_failedId);
-    }
+﻿
+$(document).ready(function(){
+	console.log("Hello, World!");
 });
