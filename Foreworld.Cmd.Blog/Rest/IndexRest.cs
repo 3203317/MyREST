16072a290441eb819fdddf65fbf74e71f48f4d4a
3203317 <3203317@qq.com>
2014-01-26 14:08:30 +0800

    haha

---
+++ Foreworld.Cmd.Blog/Rest/IndexRest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.Security;
+using System.Configuration;
+using System.Reflection;
+
+using log4net;
+using Newtonsoft.Json;
+
+using NVelocity;
+using NVelocity.Context;
+
+using Foreworld.Log;
+
+namespace Foreworld.Cmd.Blog.Rest
+{
+    public class IndexRest : BaseRest
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(IndexRest));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        [Resource]
+        public ResultMapper IndexUI(Parameter @parameter)
+        {
+            IContext vltCtx = new VelocityContext();
+            vltCtx.Put("title", "欢迎页");
+
+            HtmlObject htmlObj = new HtmlObject();
+            htmlObj.Template = GetVltTemplate();
+            htmlObj.Context = vltCtx;
+
+            ResultMapper mapper = new ResultMapper();
+            mapper.Data = htmlObj;
+            mapper.Success = true;
+            return mapper;
+        }
+    }
+}
