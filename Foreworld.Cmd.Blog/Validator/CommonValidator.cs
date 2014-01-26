1a8660b0ea9a5693c465404bc1386278e173c9f4
3203317 <3203317@qq.com>
2014-01-26 16:38:47 +0800

    haha

---
+++ Foreworld.Cmd.Blog/Validator/CommonValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.Security;
+
+using log4net;
+
+using Foreworld.Log;
+
+namespace Foreworld.Cmd.Blog.Validator
+{
+    public class CommonValidator : IValidator
+    {
+        public CommonValidator()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public override ResultMapper Validate(Parameter @parameter)
+        {
+            ResultMapper mapper = new ResultMapper();
+            mapper.Success = true;
+            return mapper;
+        }
+    }
+}
