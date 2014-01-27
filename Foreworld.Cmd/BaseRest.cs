3e9848c293804a545d0e19923e0e0960fa4d25d5
3203317 <3203317@qq.com>
2014-01-27 23:52:23 +0800

    haha

--- Foreworld.Cmd/BaseRest.cs
+++ Foreworld.Cmd/BaseRest.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.IO;
 using System.Reflection;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 using log4net;
 
@@ -36,6 +37,24 @@ namespace Foreworld.Cmd
         }
 
         /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="templateName"></param>
+        /// <returns></returns>
+        private string GetVltTemplate(string templateName)
+        {
+            string resultStr = string.Empty;
+            Stream stream = this.GetType().Assembly.GetManifestResourceStream(this.GetType().Assembly.GetName().Name + ".tpl." + templateName + ".html");
+            if (null != stream)
+            {
+                byte[] byte_3 = new byte[stream.Length];
+                stream.Read(byte_3, 0, byte_3.Length);
+                resultStr = Encoding.UTF8.GetString(byte_3); //将byte数组转换为string
+            }
+            return resultStr;
+        }
+
+        /// <summary>
         /// 获取模板
         /// </summary>
         /// <returns></returns>
@@ -51,12 +70,20 @@ namespace Foreworld.Cmd
             }
             else
             {
-                Stream stream_3 = this.GetType().Assembly.GetManifestResourceStream(this.GetType().Assembly.GetName().Name + ".tpl." + tplName + ".html");
-                if (null != stream_3)
+                resultStr = GetVltTemplate(tplName);
+
+                if (0 < resultStr.Length)
                 {
-                    byte[] byte_4 = new byte[stream_3.Length];
-                    stream_3.Read(byte_4, 0, byte_4.Length);
-                    resultStr = Encoding.UTF8.GetString(byte_4); //将byte数组转换为string
+                    Regex regex_4 = new Regex("#parse\\(\\'(.*)\\.html\\'\\);");
+                    MatchCollection matches_4 = regex_4.Matches(resultStr);
+
+                    for (int i = 0, j = matches_4.Count; i < j; i++)
+                    {
+                        string match_5 = matches_4[i].ToString();
+                        string tplName_5 = matches_4[i].Groups[1].Value.Replace('/', '.');
+
+                        resultStr = resultStr.Replace(match_5, GetVltTemplate(tplName_5));
+                    }
                 }
             }
             return resultStr;
