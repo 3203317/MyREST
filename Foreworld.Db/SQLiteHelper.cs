50a77038e7a4debaf769a37b0b99904b06c719f7
3203317 <3203317@qq.com>
2014-01-28 22:18:03 +0800

    haha

---
+++ Foreworld.Db/SQLiteHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace Foreworld.Db
+{
+    public static class SQLiteHelper
+    {
+        public static DataSet ExecuteDataSet(string connectionString, CommandType commandType, string commandText)
+        {
+            return null;
+        }
+    }
+}
