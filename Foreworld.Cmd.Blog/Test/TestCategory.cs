66e7e96f428d86492e2a048efe0bff5d25bcce61
3203317 <3203317@qq.com>
2014-01-28 21:30:11 +0800

    haha

--- Foreworld.Cmd.Blog/Test/TestCategory.cs
+++ Foreworld.Cmd.Blog/Test/TestCategory.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Web;
+using System.Security.Cryptography;
+using System.IO;
+using System.Net;
+
+using NUnit.Framework;
+using Newtonsoft.Json;
+using log4net;
+
+using Foreworld.Log;
 
 namespace Foreworld.Cmd.Blog.Test
 {
+    [TestFixture]
     class TestCategory
     {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(TestCategory));
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void test_CRUD()
+        {
+            Console.WriteLine("Hello");
+        }
     }
 }
