e43ac93998cc8a537053a61bb0940ff9c6a0941b
3203317 <3203317@qq.com>
2014-01-29 20:43:24 +0800

    haha

---
+++ Foreworld.Cmd.Blog/Model/Article.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.OleDb;
+
+namespace Foreworld.Cmd.Blog.Model
+{
+    [Table("F_ARTICLE")]
+    public class Article
+    {
+        public const string POST_TIME = "PostTime";
+
+        [Column("主键", Id = true, Length = 33, Nullable = false, Unique = true)]
+        public String Id { get; set; }
+
+        [Column("文章标题")]
+        public String ArticleTitle { get; set; }
+
+        [Column("文章介绍")]
+        public String ArticleIntro { get; set; }
+
+        [Column("文章内容")]
+        public String ArticleContent { get; set; }
+
+        [Column("文章作者")]
+        public String ArticleAuthor { get; set; }
+
+        [Column("发布时间", OleDbType = OleDbType.DBTimeStamp)]
+        public String PostTime { get; set; }
+
+
+    }
+}
