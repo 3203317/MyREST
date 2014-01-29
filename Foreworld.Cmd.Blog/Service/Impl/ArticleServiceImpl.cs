6c9c26f551410b31417298073ab22a3282c8025e
3203317 <3203317@qq.com>
2014-01-29 20:51:43 +0800

    haha

---
+++ Foreworld.Cmd.Blog/Service/Impl/ArticleServiceImpl.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using log4net;
+
+using Foreworld.Cmd.Blog.Model;
+using Foreworld.Cmd.Blog.Dao;
+using Foreworld.Cmd.Blog.Dao.Impl;
+
+namespace Foreworld.Cmd.Blog.Service.Impl
+{
+    public class ArticleServiceImpl : BaseService, ArticleService
+    {
+        private ArticleDao _articleDao;
+
+        public ArticleServiceImpl()
+        {
+            _articleDao = new ArticleDaoImpl();
+        }
+
+        private static readonly ILog _log = LogManager.GetLogger(typeof(ArticleServiceImpl));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Article> GetArticles()
+        {
+            Article __search = new Article();
+
+            Dictionary<string, string> __sort = new Dictionary<string, string>();
+            __sort.Add(Article.POST_TIME, "DESC");
+
+            List<Article> __list = _articleDao.queryAll(null, __sort, __search);
+            return __list;
+        }
+    }
+}
