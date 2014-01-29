e4dc0df1539b0c5b50cbce29e2cbd47a35cebe19
3203317 <3203317@qq.com>
2014-01-29 11:57:29 +0800

    haha

---
+++ Foreworld.Cmd.Blog/Dao/CategoryDao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foreworld.Cmd.Blog.Dao
+{
+    using Category = Foreworld.Cmd.Blog.Model.Category;
+
+    public interface CategoryDao : IDao<Category, Category>
+    {
+    }
+}
