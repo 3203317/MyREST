2134a6b8802c2e542abad4ef524168ba02e92b7f
3203317 <3203317@qq.com>
2014-02-07 11:21:55 +0800

    haha

---
+++ Foreworld.Cmd.Blog/Dao/Impl/LinkDaoImpl.cs
@@ -0,0 +1,15 @@
+﻿#define DEBUG
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using log4net;
+using Foreworld.Cmd.Blog.Model;
+
+namespace Foreworld.Cmd.Blog.Dao.Impl
+{
+    public class LinkDaoImpl : BaseDao<Link, Link>, LinkDao
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(LinkDaoImpl));
+    }
+}
