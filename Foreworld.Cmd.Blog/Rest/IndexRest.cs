44a038f143f4130bda1ad498220aa4986750528c
3203317 <3203317@qq.com>
2014-02-07 10:43:18 +0800

    haha

--- Foreworld.Cmd.Blog/Rest/IndexRest.cs
+++ Foreworld.Cmd.Blog/Rest/IndexRest.cs
@@ -49,6 +49,8 @@ namespace Foreworld.Cmd.Blog.Rest
 
             IContext vltCtx = new VelocityContext();
             vltCtx.Put("title", "FOREWORLD 洪荒");
+            vltCtx.Put("description", "个人博客");
+            vltCtx.Put("keywords", "Bootstrap3");
             vltCtx.Put("topMessage", "欢迎您。今天是" + DateTime.Now.ToString("yyyy年MM月dd日") + "。");
             vltCtx.Put("categorys", _categoryService.GetCategorys());
             vltCtx.Put("articles", _articleService.GetArticles(pagination));
