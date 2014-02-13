9501bf3e62e3741cbc57512f05ae974e829f2dc1
3203317 <3203317@qq.com>
2014-02-13 21:36:42 +0800

    haha

--- Foreworld.Cmd.Blog/Rest/IndexRest.cs
+++ Foreworld.Cmd.Blog/Rest/IndexRest.cs
@@ -175,5 +175,41 @@ namespace Foreworld.Cmd.Blog.Rest
             mapper.Success = true;
             return mapper;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        [Resource(Public = true)]
+        public ResultMapper CategoryUI(Parameter @parameter)
+        {
+            HttpRequest request = @parameter.HttpContext.Request;
+            string categoryName = request.QueryString["categoryName"];
+
+            IContext vltCtx = new VelocityContext();
+            vltCtx.Put("moduleName", "category");
+            vltCtx.Put("virtualPath", "../../");
+
+            vltCtx.Put("title", "FOREWORLD 洪荒");
+            vltCtx.Put("atitle", categoryName);
+            vltCtx.Put("categoryName", categoryName);
+            vltCtx.Put("description", "个人博客");
+            vltCtx.Put("keywords", "Bootstrap3");
+            vltCtx.Put("topMessage", "欢迎您。今天是" + DateTime.Now.ToString("yyyy年MM月dd日") + "。");
+            vltCtx.Put("categorys", _categoryService.GetCategorys());
+            vltCtx.Put("top10Comments", _commentService.GetTop10Comments());
+            vltCtx.Put("usefulLinks", _linkService.GetUsefulLinks());
+            vltCtx.Put("top10ViewNums", _articleService.GetTop10ViewNums());
+
+            HtmlObject htmlObj = new HtmlObject();
+            htmlObj.Template = GetVltTemplate();
+            htmlObj.Context = vltCtx;
+
+            ResultMapper mapper = new ResultMapper();
+            mapper.Data = htmlObj;
+            mapper.Success = true;
+            return mapper;
+        }
     }
 }
