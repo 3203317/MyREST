b711e3aa3654b6bc5050667218329e879843a656
3203317 <3203317@qq.com>
2014-02-18 13:37:56 +0800

    haha

--- Foreworld.Cmd.Blog/Rest/IndexRest.cs
+++ Foreworld.Cmd.Blog/Rest/IndexRest.cs
@@ -303,5 +303,34 @@ namespace Foreworld.Cmd.Blog.Rest
             mapper.Success = true;
             return mapper;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        [Resource(Public = true)]
+        public ResultMapper MoreTagUI(Parameter @parameter)
+        {
+            HttpRequest request = @parameter.HttpContext.Request;
+            string tagName = request.QueryString["tagName"];
+
+            string dataStr = GetDataStr(@parameter);
+            Pagination pagination = JavaScriptConvert.DeserializeObject<Pagination>(dataStr);
+            pagination.PageSize = 10;
+
+            IContext vltCtx = new VelocityContext();
+            vltCtx.Put("articles", _articleService.FindArticlesByTagName(tagName, pagination));
+            vltCtx.Put("virtualPath", "../../");
+
+            HtmlObject htmlObj = new HtmlObject();
+            htmlObj.Template = GetVltTemplate();
+            htmlObj.Context = vltCtx;
+
+            ResultMapper mapper = new ResultMapper();
+            mapper.Data = htmlObj;
+            mapper.Success = true;
+            return mapper;
+        }
     }
 }
