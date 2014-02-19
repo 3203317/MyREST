0eb0e11a9c5f34421a5f93933e8f7ceb3a56667a
3203317 <3203317@qq.com>
2014-02-19 22:23:30 +0800

    haha

---
+++ Foreworld.Cmd.Blog/Model/Archive.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foreworld.Cmd.Blog.Model
+{
+    public class Archive
+    {
+        public Int32? Y4 { get; set; }
+
+        public List<ArchiveChild> ArchiveChildren { get; set; }
+    }
+}
---
+++ Foreworld.Cmd.Blog/Model/ArchiveChild.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foreworld.Cmd.Blog.Model
+{
+    public class ArchiveChild
+    {
+        public Int32? M2 { get; set; }
+
+        public List<Article> Articles { get; set; }
+    }
+}
