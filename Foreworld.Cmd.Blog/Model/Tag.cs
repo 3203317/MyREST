e3a8fded0164268ddbe39dbc5faa473c220ce3dc
3203317 <3203317@qq.com>
2014-02-23 14:36:11 +0800

    haha

--- Foreworld.Cmd.Blog/Model/Tag.cs
+++ Foreworld.Cmd.Blog/Model/Tag.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 
+using MySql.Data.MySqlClient;
+
 namespace Foreworld.Cmd.Blog.Model
 {
+    [Table("F_TAG")]
     public class Tag
     {
+        [Column("主键", Id = true, Length = 32, Nullable = false, Unique = true, MySqlDbType = MySqlDbType.VarChar)]
+        public String Id { get; set; }
+
+        [Column("标签名称")]
         public String TagName { get; set; }
+
+        [Column("标签数量")]
+        public Int32? TagCount { get; set; }
     }
 }
